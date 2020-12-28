570bdd425b2185a28174471655efbd9260d5ea2c
darkyzhou <darkyoooooo@gmail.com>
2020-12-29 00:19:58 +0800

    feat: add elasticsearch

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const KAFKA_BROKERS = process.env.KAFKA_BROKERS;
 const INFLUX_URL = process.env.INFLUX_URL;
 const INFLUX_TOKEN = process.env.INFLUX_TOKEN;
 const MONGODB_URL = process.env.MONGODB_URL;
+const ELASTICSEARCH_URL = process.env.ENV_ELASTICSEARCH_URL;
 const UPDATE_USD_EXCHANGE_RATE_INTERVAL =
   Number(process.env.UPDATE_USD_EXCHANGE_RATE_INTERVAL) || 60 * 60 * 3000; // 3 hours
 const HOSTNAME = process.env.HOSTNAME; // offered by kubernetes automatically
@@ -15,6 +16,7 @@ const { addExitHook } = require('exit-hook-plus');
 const { Kafka } = require('kafkajs');
 const { InfluxDB } = require('@influxdata/influxdb-client');
 const { MongoClient } = require('mongodb');
+const { Client } = require('@elastic/elasticsearch');
 const { updateUsdExchangeRateMapping } = require('./lib/superchat-amount-utils');
 const { verifyChannelInfo, collectChannelInfo } = require('./lib/collect-channel-info');
 const { verifyLivestreamInfo, collectLivestreamInfo } = require('./lib/collect-livestream-info');
@@ -26,11 +28,9 @@ const kafka = new Kafka({
   brokers: KAFKA_BROKERS.trim().split(',')
 });
 const kafkaDataConsumer = kafka.consumer({ groupId: 'data-collector', sessionTimeout: 12 * 1000 });
-const mongo = new MongoClient(MONGODB_URL);
-const influx = new InfluxDB({
-  url: INFLUX_URL,
-  token: INFLUX_TOKEN
-});
+const mongo = new MongoClient(MONGODB_URL, { useUnifiedTopology: true });
+let influx;
+let elasticClient;
 
 const COLLECTOR_ITEMS = {
   'channel-info': [verifyChannelInfo, collectChannelInfo],
@@ -57,19 +57,32 @@ async function init() {
   const videoInfoCollection = db.collection('video-info');
   const livestreamInfoCollection = db.collection('livestream-info');
 
+  console.info('loading USD exchange rate mapping from mongodb for the first time');
   await initUsdExchangeRateUpdating(usdExchangeRateCollection);
 
-  console.info('preparing influxdb write apis');
+  console.info('connecting to influxdb');
+  influx = new InfluxDB({
+    url: INFLUX_URL,
+    token: INFLUX_TOKEN
+  });
   const channelStatsBucket = influx.getWriteApi('vtuberstats', 'channel-stats', 'ms');
+  addExitHook(async () => await ctx.channelStatsBucket.close());
   const videoStatsBucket = influx.getWriteApi('vtuberstats', 'video-stats', 'ms');
+  addExitHook(async () => await ctx.videoStatsBucket.close());
   const livestreamStatsBucket = influx.getWriteApi('vtuberstats', 'livestream-stats', 'ms');
+  addExitHook(async () => await ctx.livestreamStatsBucket.close());
   const livechatMessageBucket = influx.getWriteApi('vtuberstats', 'livechat-messages', 'ms');
+  addExitHook(async () => await ctx.livechatMessageBucket.close());
   const livechatSuperchatMessageBucket = influx.getWriteApi(
     'vtuberstats',
     'livechat-superchat-messages',
     'ms'
   );
-  addExitHook(async () => await ctx.channelStatsBucket.close());
+  addExitHook(async () => await ctx.livechatSuperchatMessageBucket.close());
+
+  console.info('connecting to elasticsearch');
+  elasticClient = new Client({ node: ELASTICSEARCH_URL });
+  addExitHook(async () => await elasticClient.close());
 
   const ctx = {
     channelStatsBucket,
@@ -79,7 +92,8 @@ async function init() {
     livechatSuperchatMessageBucket,
     channelInfoCollection,
     videoInfoCollection,
-    livestreamInfoCollection
+    livestreamInfoCollection,
+    elasticClient
   };
 
   console.info('start reading data from kafka');
@@ -121,7 +135,6 @@ async function readMessageFromKafka(collectorCtx) {
 }
 
 async function initUsdExchangeRateUpdating(collection) {
-  console.info('loading USD exchange rate mapping from mongodb for the first time');
   await updateUsdExchangeRateMapping(collection);
   const action = async () => {
     console.info('updating loaded USD exchange rate mapping');
